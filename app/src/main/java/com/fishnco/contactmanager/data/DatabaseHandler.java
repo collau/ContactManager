c3e999f27d60b617c18457ffda4bf73cba3f2d71
junyi <collau@hotmail.com>
2021-06-07 23:23:35 +0800

    2. Database Handler Class Setup

--- app/src/main/java/com/fishnco/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/fishnco/contactmanager/data/DatabaseHandler.java
@@ -10,6 +10,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.fishnco.contactmanager.util.Utility;
+
 public class DatabaseHandler extends SQLiteOpenHelper {
 
     /**
@@ -23,13 +25,39 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         super(context, name, factory, version);
     }
 
+    public DatabaseHandler(Context context) {
+        super(context, Utility.DATABASE_NAME, null, Utility.DATABASE_VERSION);
+    }
+
+    //We create our table
     @Override
     public void onCreate(SQLiteDatabase db) {
+        //SQL - Structured Query Language
 
+        /*
+         * CREATE TABLE contacts (
+         *      id INTEGER PRIMARY KEY,
+         *      name TEXT,
+         *      contacts TEXT
+         * );
+         */
+        String CREATE_CONTACT_TABLE = "CREATE TABLE " + Utility.TABLE_NAME + "("
+                + Utility.KEY_ID + " INTEGER PRIMARY KEY, "
+                + Utility.KEY_NAME + " TEXT, "
+                + Utility.KEY_PHONENO + " TEXT" + ")";
+
+        //Creating our table
+        db.execSQL(CREATE_CONTACT_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+        //Delete whole table and create again
+        String DROP_TABLE = "DROP TABLE IF EXISTS " + Utility.DATABASE_NAME;
+        db.execSQL(DROP_TABLE);
+
+        //Create a table again
+        onCreate(db);
     }
 }
